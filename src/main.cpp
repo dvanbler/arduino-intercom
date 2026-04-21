abf705e3e17b5488977e2902889e2fc1a8ddaa63
Derek Van Blerkom <dvanbler@users.noreply.github.com>
2026-04-20 21:49:07 -0400

    Working speaker and mic streaming

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #include "MicDriver.h"
 #include "secrets.h"
 
-#define UDP_PORT 6769
+#define RECEIVE_UDP_PORT 6769
+#define SEND_UDP_PORT 6769
 #define FREQ_HZ 32000.0
 
 #define SPEAKER_PIN A0
@@ -16,12 +17,16 @@
 #define MIC_DMA_CHANNEL 1
 
 SpeakerDriver speaker(FREQ_HZ, SPEAKER_PIN, SPEAKER_DMA_CHANNEL);
-SpeakerDriver::BufferPtr buffers = speaker.get_buffers();
+SpeakerDriver::BufferPtr speaker_buffers = speaker.get_buffers();
 
 MicDriver mic(FREQ_HZ, MIC_PIN, MIC_DMA_CHANNEL);
+MicDriver::BufferPtr mic_buffers = mic.get_buffers();
 
 IPAddress ip;
 WiFiUDP udp;
+
+IPAddress last_receive_from_ip;
+bool has_ip = false;
 
 int init_wifi() {
     if (WiFi.status() == WL_NO_MODULE) {
@@ -45,7 +50,7 @@ int init_wifi() {
         ip = WiFi.localIP();
     } while (ip == INADDR_NONE);
 
-    udp.begin(UDP_PORT);
+    udp.begin(RECEIVE_UDP_PORT);
 
     return 0;
 }
@@ -92,24 +97,45 @@ void setup() {
 }
 
 void loop() {
+    static unsigned long last_ip_check = 0;
+
     static unsigned long last = 0;
     unsigned long now = millis();
 
-    if (digitalRead(D4) == LOW) {
+    if (digitalRead(D4) == HIGH) {
+        // Default operation - play received audio stream from network
         int buffer_num = speaker.reserve_buffer();
         if (buffer_num >= 0) {
             int available = udp.parsePacket();
             if (available == SpeakerDriver::BUFFER_LEN) {
-                udp.read(buffers[buffer_num], available);
+                if (!has_ip || now - last_ip_check > 10000) {
+                    // periodically update the ip address to the latest sending remote
+                    last_receive_from_ip = udp.remoteIP();
+                    has_ip = true;
+                    last_ip_check = now;
+                    Serial.println(last_receive_from_ip);
+                }
+                udp.read(speaker_buffers[buffer_num], available);
                 speaker.release_buffer(buffer_num, true);
             } else {
                 speaker.release_buffer(buffer_num, false);
             }
         }
+    } else if (last_receive_from_ip != INADDR_NONE) {
+        // Button is pressed, and we have a target IP: Stream mic audio
+        int buffer_num = mic.reserve_buffer_for_read();
+        if (buffer_num >= 0) {
+            udp.beginPacket(last_receive_from_ip, SEND_UDP_PORT);
+            udp.write(mic_buffers[buffer_num], MicDriver::BUFFER_LEN);
+            udp.endPacket();
+            mic.release_buffer(buffer_num, true);
+        }
     }
 
+    /*
     if (now - last > 1000) {
         last = now;
         mic.print_debug();
     }
+    */
 }
